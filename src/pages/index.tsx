9e01c7bd783bb81b55a41f8368a7c6d848462d50
helpingwizard <vctanish7@gmail.com>
2024-02-15 18:16:01 +0530

    v1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import ProfileAbout from "./profileAbout";
 import PostPage from "./postPage";
+import PostPg from "./postpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <PostPage></PostPage>
+    <div>
+    
+    <PostPage/>
+    </div>
   );
 }
