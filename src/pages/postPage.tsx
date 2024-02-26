5551652ebb56d7458440a3a2f5e9c23bffe62754
soham kottawar <sohamkottawar2004@gmail.com>
2024-02-26 20:56:00 +0530

    allignment of component

--- src/pages/postPage.tsx
+++ src/pages/postPage.tsx
@@ -16,13 +16,13 @@ export default function PostPage() {
                 <div>
                     <div className="flex">
                         <div>
-                            <div className="relative w-20 h-20 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                                <svg className="absolute w-20 h-20 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+                            <div className="relative w-20 h-20 overflow-hidden bg-gray-500 rounded-full dark:bg-gray-700        ">
+                                <svg className="absolute w-20 h-20 text-gray-400 -left" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
                             </div>
                         </div>
 
                         <div className="ml-3">
-                            <p className="text-xl font-bold text-black">Tanish Charthankar</p>
+                            <p className="text-xl font-bold text-black">Soham Kottawar</p>
                             <p className="text-lg font-semibold text-blue-500">Full Stack Dev</p>
                             <p className="text-sm text-gray-500">Mountain view, California</p>
 
