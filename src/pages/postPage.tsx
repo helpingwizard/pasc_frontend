9918afa3ba129862ed4865008dda008fe3e40d5f
soham kottawar <sohamkottawar2004@gmail.com>
2024-03-01 00:29:26 +0530

    added the domain box solved  placement of boxes

--- src/pages/postPage.tsx
+++ src/pages/postPage.tsx
@@ -10,8 +10,8 @@ export default function PostPage() {
             </button>
         </div>
 
-        <div className="max-w-10xl">
-            <div className="block max-w-10xl p-6 bg-slate-100 rounded-lg shadow-lg">
+        <div className="max-w-3/4">
+            <div className="block max-w-3/4 p-6 bg-slate-100 rounded-lg shadow-lg">
 
                 <div>
                     <div className="flex">
@@ -29,25 +29,32 @@ export default function PostPage() {
                         </div>
                     </div>
 
-                    <div className="mt-4">
+                    <div className="mt-4 w-full">
                         <input
                             type="text"
                             id="title-input"
                             placeholder="Title"
-                            className="bg-slate-200 border text-black text-lg placeholder-gray-500 rounded-lg focus:ring-blue-500 focus:border-blue-1000 block w-full p-2.5 pl-8 xl:w-96 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="bg-slate-200 border text-black text-lg placeholder-gray-500 rounded-lg focus:ring-blue-500 focus:border-blue-1000 block w-full p-2.5 pl-8 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                        <br />
+                        <input
+                            type="text"
+                            id="title-input"
+                            placeholder="Genre(Domain)"
+                            className="bg-slate-200 border text-black text-lg placeholder-gray-500 rounded-lg focus:ring-blue-500 focus:border-blue-1000 block w-full p-2.5 pl-8  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         />
                         <br />
                         <textarea
                             id="thoughts-input"
                             placeholder="What you want to Talk about?"
-                            className="bg-slate-200 border text-black text-base placeholder-gray-500 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-8 xl:w-96 h-96 resize-none dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="bg-slate-200 border text-black text-base placeholder-gray-500 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-8  h-80 resize-none dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         ></textarea>
                     </div>
 
 
 
 
-                    <div className="flex justify-between mt-3">
+                    <div className="flex justify-between mt-8">
                         <div className="flex justify-start space-x-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className="w-9 h-9">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
@@ -71,11 +78,12 @@ export default function PostPage() {
 
 
                         <div>
-                            <button
+
+                        <button
                             type="button"
                             className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-7 py-1 text-center me-2 mb-2 border-none"
                             >
-                            Post
+                            Add Media
                             </button>
 
                             <button
@@ -84,13 +92,17 @@ export default function PostPage() {
                             >
                             Save draft
                             </button>
-
+                            
                             <button
                             type="button"
                             className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-7 py-1 text-center me-2 mb-2 border-none"
                             >
-                            Add Media
+                            Post
                             </button>
+
+                            
+
+                         
 
 
                         </div>
