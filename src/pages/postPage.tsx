2fda5ab7e78f5c080c41428d71360bcbbc597272
helpingwizard <vctanish7@gmail.com>
2024-03-24 04:26:00 +0530

    added updated UI and api integration

--- src/pages/postPage.tsx
+++ src/pages/postPage.tsx
@@ -3,8 +3,8 @@ import { useRouter } from "next/router"
 import { useState } from "react";
 
 export default function PostPage() {
-    const router = useRouter();
-    const {user_id} = router.query;
+    
+    
     const [title, setTitle] = useState("");
     const [domain, setDomain] = useState("");
     const [content, setContent] = useState("");
@@ -34,124 +34,55 @@ export default function PostPage() {
     
     
       
+    
     return (
-        <div className="flex justify-center">
-    <div>
-        <div className="mt-32">
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="Black" className="w-8 h-8">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
-                </svg>
-            </button>
-        </div>
-
-        <div className="max-w-3/4">
-            <div className="block max-w-3/4 p-6 bg-slate-100 rounded-lg shadow-lg">
-
-                <div>
-                    <div className="flex">
-                        <div>
-                            <div className="relative w-20 h-20 overflow-hidden bg-gray-500 rounded-full dark:bg-gray-700        ">
-                                <svg className="absolute w-20 h-20 text-gray-400 -left" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
-                            </div>
-                        </div>
-
-                        <div className="ml-3">
-                            <p className="text-xl font-bold text-black">Tanish Charthankar</p>
-                            <p className="text-lg font-semibold text-blue-500">Full Stack Dev</p>
-                            <p className="text-sm text-gray-500">Mountain view, California</p>
-
-                        </div>
-                    </div>
-
-                    <div className="mt-4 w-full">
-                        <input
-                        onChange={(e)=>{setTitle(e.target.value)}}
-                            type="text"
-                            id="title-input"
-                            placeholder="Title"
-                            className="bg-slate-200 border text-black text-lg placeholder-gray-500 rounded-lg focus:ring-blue-500 focus:border-blue-1000 block w-full p-2.5 pl-8 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        />
-                        <br />
-                        <input
-                            type="text"
-                            id="title-input"
-                            placeholder="Genre(Domain)"
-                            className="bg-slate-200 border text-black text-lg placeholder-gray-500 rounded-lg focus:ring-blue-500 focus:border-blue-1000 block w-full p-2.5 pl-8  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        />
-                        <br />
-                        <textarea
-                        onChange={(e) => setContent(e.target.value)}
-                            id="thoughts-input"
-                            placeholder="What you want to Talk about?"
-                            className="bg-slate-200 border text-black text-base placeholder-gray-500 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-8  h-80 resize-none dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        ></textarea>
-                    </div>
-
-
-
-
-                    <div className="flex justify-between mt-8">
-                        <div className="flex justify-start space-x-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className="w-9 h-9">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
-                            </svg>
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className="w-9 h-9">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
-                            </svg>
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className="w-9 h-9">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
-                            </svg>
-
-
-
-
-
-                        </div>
-
-
-
-                        <div>
-
-                        <button
-                            type="button"
-                            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-7 py-1 text-center me-2 mb-2 border-none"
-                            >
-                            Add Media
-                            </button>
-
-                            <button
-                            type="button"
-                            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-7 py-1 text-center me-2 mb-2 border-none"
-                            >
-                            Save draft
-                            </button>
-                            
-                            <button
-                            onClick={handleSubmit}
-                            type="button"
-                            className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-7 py-1 text-center me-2 mb-2 border-none"
-                            >
-                            Post
-                            </button>
-
-                            
-
-                         
-
-
-                        </div>
+        <div className="flex justify-center mt-40">
+            
+            <div className="w-full md:max-w-3xl rounded overflow-hidden shadow-lg">
+                
+                <div className="px-6 py-4">
+                   
+                    <div className="mb-4">
+                        
+                    <input onChange={(e) => setTitle(e.target.value)} className="shadow appearance-none border border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Title"/>
 
                     </div>
                 </div>
 
+                <div className="px-6 py-4">
+                   
+                    <div className="mb-4">
+                        
+                        <input className="shadow appearance-none border  border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Domain"/>
+                    </div>
+                </div>
+
+                <div className="px-6 py-4">
+                    <div className="mb-4">
+                        <textarea
+                        onChange={(e) => setContent(e.target.value)}
+                            className="h-72 md:h-96 shadow appearance-none border  border-black rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username"
+                            placeholder="Content"
+                        />
+                    </div>
+                </div>
+
+                <div className="px-6 pt-4 pb-2 flex justify-end space-x-9">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 border border-blue-700 rounded">
+                    Add media
+                    </button>
+
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 border border-blue-700 rounded">
+                    Save draft
+                    </button>
+
+                    <button onClick= {handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 border border-blue-700 rounded">
+                    Post
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-
-    )
+    );
+        
 }
